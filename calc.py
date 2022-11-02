9fb4f673e48b0e14c778fe1fc71ee9f01d08ea9b
shubhammishra-2020 <shubham.mishra@sjsu.edu>
2022-11-02 13:52:58 -0700

    finished 1,2,3,5

--- calc.py
+++ calc.py
@@ -1,2 +1,22 @@
 def calculator(value):
+    valInput = []
+
+    for i in range(len(value)):  # Push values to stack
+        if(value[i] != ' '): # Accounts for spacing
+            if(value[i].isdigit()):
+                valInput.push(value[i])
+            elif(value[i])
+
     
+
+
+        
+
+
+    
+        
+  
+
+calculator('8 + 8')
+
+        
--- sort.py
+++ sort.py
@@ -1,8 +1,12 @@
 def sort_dictionary(values):
     values = sorted(values.items(), key=lambda item: item[1][1])
-    
-
-        
+    print("{ ", end="")
+    for index, item in enumerate(values):
+        if(index == len(values) - 1):
+            print('(\'{Name}\',{Phone})'.format(Name=item[0],Phone=item[1][0]), end=" ")
+        else:
+           print('(\'{Name}\',{Phone})'.format(Name=item[0],Phone=item[1][0]), end=", ") 
+    print("}")    
 test =  {'Tom': (5464512, 24), 'Sara':(5446987, 32), 'Mary' : (1557896, 20)}
 
 sort_dictionary(test)
